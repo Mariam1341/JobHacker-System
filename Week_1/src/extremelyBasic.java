433a21a249a7de863e0a9db779e3932bd46a3324
Mariam1341 <mmm01061480913@gmail.com>
2023-04-07 16:37:29 +0200

    1st changes req

--- Week_1/src/extremelyBasic.java
+++ Week_1/src/extremelyBasic.java
@@ -5,11 +5,11 @@ public class Extremely_Basic {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
 
-        int A = input.nextInt();
-        int B = input.nextInt();
+        int a = input.nextInt();
+        int b = input.nextInt();
 
-        int X = A + B;
+        int x = a + b;
 
-        System.out.println("X = " + X);
+        System.out.println("X = " + x);
     }
 }
