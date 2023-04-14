4643f883de4a59b3a4f7f8a9e3bb94d96d593bdd
Mariam1341 <mmm01061480913@gmail.com>
2023-04-14 05:31:52 +0200

    ,

--- Week_2/src/probTwo.java
+++ Week_2/src/probTwo.java
@@ -3,12 +3,11 @@ public class probTwo {
     public static int[] divisibleByThree (){
 
         int j = 0;
-        int[] result = new int[50];
+        int[] result = new int[33];
 
         for (int i = 1; i <= 100; i++){
             if(i % 3 == 0){
                 result [j] = i;
-                System.out.println(i);
                 j++;
             }
         }
@@ -22,9 +21,9 @@ public class probTwo {
 
         int[] arr =  divisibleByThree();
 
-//        for (int i : arr){
-//            System.out.println(arr[i]);
-//        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
 
     }
 }
